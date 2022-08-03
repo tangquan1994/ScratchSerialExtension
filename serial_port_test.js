fd97496195d38457c0cf6a538023fad8d0c3d921
tangquan <tangquan@baidu.com>
2022-08-03 21:55:30 +0800

    use serial port

--- serial_port_test.js
+++ serial_port_test.js
@@ -2,8 +2,23 @@
 const bindings_cpp_1 = require("@serialport/bindings-cpp");
 var SerialPort = require('serialport').SerialPort
 
-const serialport = new SerialPort({ path: 'COM8', baudRate: 9600 })
+//获取端口列表
+SerialPort.list(function (error, ports) {
+    console.log('port list:');
+    ports.forEach(function(port) {
+        console.log(port.comName);
+        console.log(port.pnpId);
+        console.log(port.manufacturer);
+    });
+})
+
+const serialport = new SerialPort({ path: 'COM5', baudRate: 9600 })
 serialport.write('ROBOT POWER ON')
+
+serialport.on('data', async function (data) {
+	//接收到串口传递来的数据后，对数据处理
+    console.log('data received: '+data)
+})
 
 // (0, bindings_cpp_1.autoDetect)()
 //     .list()
@@ -15,14 +30,13 @@ serialport.write('ROBOT POWER ON')
 //     process.exit(1);
 // });
 
-bindings_cpp_1.WindowsBinding.list().then(ports => {
-        console.log(ports)
-    })
-    .catch(err => {
-    console.error(err);
-    process.exit(1);
-});
+// bindings_cpp_1.WindowsBinding.list().then(ports => {
+//         console.log(ports)
+//     })
+//     .catch(err => {
+//     console.error(err);
+//     process.exit(1);
+// });
 
-// bindings_cpp_1.WindowsBinding.
 
 
