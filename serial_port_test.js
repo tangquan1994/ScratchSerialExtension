974cc498aae5f7803d1948ee5d8bf9235db986cb
tangquan <tangquan@baidu.com>
2022-08-03 22:39:28 +0800

    nothing serious

--- serial_port_test.js
+++ serial_port_test.js
@@ -2,24 +2,6 @@
 const bindings_cpp_1 = require("@serialport/bindings-cpp");
 var SerialPort = require('serialport').SerialPort
 
-//获取端口列表
-SerialPort.list(function (error, ports) {
-    console.log('port list:');
-    ports.forEach(function(port) {
-        console.log(port.comName);
-        console.log(port.pnpId);
-        console.log(port.manufacturer);
-    });
-})
-
-const serialport = new SerialPort({ path: 'COM5', baudRate: 9600 })
-serialport.write('ROBOT POWER ON')
-
-serialport.on('data', async function (data) {
-	//接收到串口传递来的数据后，对数据处理
-    console.log('data received: '+data)
-})
-
 // (0, bindings_cpp_1.autoDetect)()
 //     .list()
 //     .then(ports => {
@@ -38,5 +20,33 @@ serialport.on('data', async function (data) {
 //     process.exit(1);
 // });
 
+
+//获取端口列表
+SerialPort.list(function (error, ports) {
+    console.log('port list:');
+    ports.forEach(function(port) {
+        console.log(port.comName);
+        console.log(port.pnpId);
+        console.log(port.manufacturer);
+    });
+})
+
+const port = new SerialPort({ path: 'COM3', baudRate: 9600 })
+
+port.open((err)=>{
+    console.log('IsOpen:',port.isOpen)
+    console.log('err:',err)
+})
+
+port.write('ROBOT POWER ON')
+
+port.on('data', async function (data) {
+    //接收到串口传递来的数据后，对数据处理
+    console.log('data received: '+data)
+})
+
+port.on('error', function (error) {
+    console.log('error: ' + error)
+})
 
 
