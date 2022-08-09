2f043d5dc24437a0b9166219c9950134fbc52bb0
tangquan <tangquan@baidu.com>
2022-08-09 11:33:40 +0800

    embed a file server into websocket server

--- nodeServer_webs.js
+++ nodeServer_webs.js
@@ -1,17 +1,71 @@
+//****************************************************************************
+//**************************nodejs websocket server***************************
+//****************************************************************************
 const express = require('express')
-const http = require('http')
+// const http = require('http')
 const socketIo = require('socket.io')
 const cors = require('cors')
 const ws = require("nodejs-websocket") // Scream server example: "hi" -> "HI!!!"
 
-var server = ws.createServer(function (conn) {  
-    console.log("New connection")  
-    conn.on("text", function (str) {    
-        console.log("Received "+str)    
-        conn.sendText(str.toUpperCase()+"!!!")  
-    })  
-    
-    conn.on("close", function (code, reason) {    
-        console.log("Connection closed")  
+var server = ws.createServer(function (conn) {
+    console.log("New connection")
+    conn.on("text", function (str) {
+        console.log("Received " + str)
+        conn.sendText(str.toUpperCase() + "!!!")
+    })
+
+    conn.on("close", function (code, reason) {
+        console.log("Connection closed")
     })
 }).listen(8000)
+
+console.log('Websocket server started');
+
+
+//****************************************************************************
+//*****************************nodejs file server*****************************
+//****************************************************************************
+var http = require('http');
+var fs = require('fs');//引入文件读取模块
+
+var documentRoot = '.';
+//需要访问的文件的存放目录
+
+var server = http.createServer(function (req, res) {
+
+    var url = req.url;
+    //客户端输入的url，例如如果输入localhost:8888/index.html
+    //那么这里的url == /index.html 
+
+    var file = documentRoot + url;
+    //   console.log(url);
+    console.log(file);
+    //E:/PhpProject/html5/websocket/www/index.html 
+
+
+    fs.readFile(file, function (err, data) {
+        /*
+          一参为文件路径
+          二参为回调函数
+            回调函数的一参为读取错误返回的信息，返回空就没有错误
+            二参为读取成功返回的文本内容
+        */
+        if (err) {
+            res.writeHeader(404, {
+                'content-type': 'text/html;charset="utf-8"'
+            });
+            res.write('<h1>404错误</h1><p>你要找的页面不存在</p>');
+            res.end();
+        } else {
+            res.writeHeader(200, {
+                'content-type': 'text/html;charset="utf-8"'
+            });
+            res.write(data);//将index.html显示在客户端
+            res.end();
+        }
+
+    });
+
+}).listen(80);
+
+console.log('File server started');
